3866d9b2d6e0649a8437d37069f815e230afcab5
nickonyi <nickoluoch1130@gmail.com>
2026-04-30 18:50:19 +0300

    feat: add Prisma schema, migrations, and initial script for user and article models

---
+++ script.js
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const user = await prisma.user.create({
+    data: {
+      name: "Johndoe",
+      email: "johndoe@email.com",
+    },
+  });
+
+  console.log(user);
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
